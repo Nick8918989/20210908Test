c3f55d3568ca9a683a38bfb7c4fae6e818c58f99
Nick <v0954788@gmail.com>
2021-09-09 15:15:42 +0800

    Dapper CRU沒有D

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Dapper;
 using FileService;
 using FileService.Models;
 using LogService;
@@ -16,21 +19,34 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Start ...");
+            
+            //資料庫連線
+            string cnStr = "Server=192.168.5.162;Database=Test;User ID=sa;Pwd=24drs;MultipleActiveResultSets=True;Max Pool Size=256;";
+
+            //測試-Dapper連線資料庫實作
+            //QueryUser(cnStr);
+
+            //測試-Dapper新增資料
+            //InsertUser(cnStr);
+
+            //測試-Dapper更新資料
+            UpdateUser(cnStr);
+
+            //測試-檔案上傳功能
+            //SetFile(cnStr).Wait();
+
             Console.ReadLine();
-            //SetFile().Wait();
         }
 
-        public static async Task SetFile()
+        public static async Task SetFile(string _cnStr)
         {
-            //資料庫連線
-            string CONNECTION_STRING = "Server=192.168.5.162;Database=HOME_CARE_TEST;User ID=sa;Pwd=24drs;MultipleActiveResultSets=True;Max Pool Size=256;";
             //Ser放檔案的資料夾
             string BASE_PATH = "C:\\Users\\Admin\\Desktop\\date";
             //建立Log實例
-            var logHelp = LogHelper.GetInstance(LogProvider.SQL, CONNECTION_STRING);
+            var logHelp = LogHelper.GetInstance(LogProvider.SQL, _cnStr);
             //建立檔案實例
-            var fileHelp = FileService.StorageHelper.GetInstance(StorageProvider.NativeFileWithSQL, CONNECTION_STRING, logHelp, BASE_PATH);
-            
+            var fileHelp = FileService.StorageHelper.GetInstance(StorageProvider.NativeFileWithSQL, _cnStr, logHelp, BASE_PATH);
+
             //測試資料來源
             string base64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
             //正則表達式-抓取Base64碼
@@ -53,6 +69,48 @@ namespace ConsoleApp1
                 {
                     Console.WriteLine("檔案上傳完成...");
                 }
+            }
+        }
+
+        public static void QueryUser(string _cnStr)
+        {
+            using (var cn = new SqlConnection(_cnStr))
+            {
+                var userList = cn.Query("SELECT * FROM MVC_DEMO_USER");
+                foreach (var user in userList)
+                {
+                    Console.WriteLine("資料是...{0}", user.ID);
+                }
+            }
+        }
+
+        public static void InsertUser(string _cnStr)
+        {
+            using (var cn = new SqlConnection(_cnStr))
+            {
+                string strSql =
+                    "INSERT INTO MVC_DEMO_USER(GROUP_ID, NAME, MAIL, PASSWORD, USERNAME) VALUES (@GROUP_ID,@NAME,@MAIL,@PASSWORD,@USERNAME);";
+                dynamic[] datas = new[] {
+                    new { GROUP_ID = 4, NAME = "上班做蝦皮5", MAIL = "test5@gmail.com", PASSWORD = "005", USERNAME = "沒有5" },
+                    new { GROUP_ID = 4, NAME = "上班做蝦皮6", MAIL = "test6@gmail.com", PASSWORD = "006", USERNAME = "沒有6" },
+                };
+                cn.Execute(strSql, datas);
+                Console.WriteLine("資料新增成功");
+            }
+        }
+
+        public static void UpdateUser(string _cnStr)
+        {
+            using (var cn = new SqlConnection(_cnStr))
+            {
+                string strSql =
+                    "UPDATE  MVC_DEMO_USER SET NAME = @NAME WHERE ID = @ID;";
+                dynamic[] updateData = new[] { 
+                    new { ID = 93, NAME = "還是上班做蝦皮1" },
+                    new { ID = 94, NAME = "還是上班做蝦皮2" },
+                };
+                cn.Execute(strSql, updateData);
+                Console.WriteLine("資料更新成功");
             }
         }
     }
